deb5132dea8a4c0c2d5bbad6ca49f6949187e568
esegebart <36378900+esegebart@users.noreply.github.com>
2020-10-25 13:04:20 -0500

    Added vector drawable support for older version compatibility. Updated the Image tag to include app:srcCompat. Added app namespace to xml.

--- app/src/main/java/com/elyse/diceroller/MainActivity.kt
+++ app/src/main/java/com/elyse/diceroller/MainActivity.kt
@@ -3,11 +3,14 @@ package com.elyse.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,15 +20,24 @@ class MainActivity : AppCompatActivity() {
             // Method that will be called when the button is clicked
             rollDice()
         }
+        // Make the dice_image ImageView show the dice image
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        // Make the result_text TextView show the random integer
-        val resultText: TextView = findViewById(R.id.result_text)
         // Get a random integer from 1-6 in rollDice
         val randomInt = Random().nextInt(6) + 1
-
-        resultText.text = randomInt.toString()
+        // Choose the correct image based on the number using a when expression
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Set the image of the dice of drawable resource
+        diceImage.setImageResource(drawableResource)
     }
 
 
